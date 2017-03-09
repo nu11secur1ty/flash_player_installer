e4938620945590c7263ba446ae9d0b9daf766034
nu11secur1ty <systadmi@abv.bg>
2017-03-10 00:35:30 +0200

    Update flashPinstaller.sh

--- flashPinstaller.sh
+++ flashPinstaller.sh
@@ -3,7 +3,8 @@ echo -e "\n\e[01;32m[+]\e[00m Preparing flash player plugin"
 cd /opt/
   git clone https://github.com/nu11secur1ty/flash_player_installer.git
   cd flash_player_installer/
-  bash finstaller.sh
-  cd 
+  cp libflashplayer.so /usr/lib/mozilla/plugins/
+  cp libflashplayer.so /usr/lib64/mozilla/plugins/
+  cd /opt/
   rm -rf /opt/flash_player_installer/
 exit 0;
